0805f0cf5ca7f78b73cb747a45d931cebe4a41f4
HirokiDaichi <HirokiDaichi@af8cfeb2-8ac1-11dd-abc6-8b02bc135b05>
2008-10-12 14:36:25 +0000

    -

--- html_template_js/build.sh
+++ html_template_js/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo 'create production code (./bin/minify)'
-
-cat template.js component.js |./bin/minify  > template_production.js
+cat license > template_production.js
+cat template.js component.js |./bin/minify  >> template_production.js
 
 echo 'create document html'
 
