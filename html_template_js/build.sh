7fab8c76594c458ddf31d75746118937166c229c
HirokiDaichi <HirokiDaichi@af8cfeb2-8ac1-11dd-abc6-8b02bc135b05>
2008-10-12 08:39:22 +0000

    -

--- html_template_js/build.sh
+++ html_template_js/build.sh
@@ -4,6 +4,5 @@ echo 'create production code (./bin/minify)'
 ./bin/minify < template.js > template_production.js
 
 echo 'create document html'
-perl -MPod::Simple::HTML -e Pod::Simple::HTML::go template.js >docs.html
-
+pod2text template.js > doc.txt
 
