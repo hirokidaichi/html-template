77e0b180cbaf2f9be4ad7e0ae644e64b71f1a3ab
hirokidaichi <hirokidaichi@af8cfeb2-8ac1-11dd-abc6-8b02bc135b05>
2009-02-06 09:20:53 +0000

    -

--- html_template_js/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'create production code (./bin/minify)'
-cat license > production/html/template.js
-cat ./src/event_wrapper.js ./src/html/template.js  | ./bin/minify  >> production/html/template.js
-
-
